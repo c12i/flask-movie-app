fc87ecc27b77a3c71e81eef528d555cf6410b21d
Collins Muriuki <murerwacollins@gmail.com>
2019-10-12 10:51:58 +0300

    Add unittest

--- manage.py
+++ manage.py
@@ -5,5 +5,14 @@ app = create_app("development")
 manager = Manager(app)
 manager.add_command("server", Server)
 
+@manager.command
+def test():
+    """
+    Run the unittests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
     manager.run()
