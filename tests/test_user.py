2d5345dc79ea1c3d9b4efe54b552b2b7a031ca1f
Collins Muriuki <murerwacollins@gmail.com>
2019-10-17 16:21:06 +0300

    Test User class successfuly

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,26 @@ from app.models import User
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
+        """
+         method that creates an instance of our User class
+        """
         self.new_user = User(password = "banana")
 
     def test_password_setter(self):
+        """
+         ascertains that when password is being hashed and the pass_secure contains a value
+        """
         self.assertTrue(self.new_user.pass_secure is not None)
+    
+    def test_no_access_password(self):
+        """
+         confirms that our application raises an AttributeError when we try and access the password property
+        """
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        """
+        confirms that our password_hash can be verified when we pass in the correct password.
+        """
+        self.assertTrue(self.new_user.verify_password('banana'))
