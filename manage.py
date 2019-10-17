beca53f6464b10e606110a67bbc37c67ae2e7af2
Collins Muriuki <murerwacollins@gmail.com>
2019-10-17 14:07:57 +0300

    db migrations 🤔

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User, Role
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app("development")
 manager = Manager(app)
 manager.add_command("server", Server)
+
+migrate = Migrate(app,db)
+manager.add_command("db",MigrateCommand)
 
 @manager.command
 def test():
