19d7a859ceb00c7cf12dc1582afa5f6f1f400b62
Collins Muriuki <murerwacollins@gmail.com>
2019-10-18 15:17:50 +0300

    Add registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import auth
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route("/register", methods = ["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for(".login"))
+        title = "New Account"
+    return render_template("auth/register.html", registration_form = form)
