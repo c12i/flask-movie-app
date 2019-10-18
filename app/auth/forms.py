bab306c6217684f3571ae0f89c3a8ccaa7506f6d
Collins Muriuki <murerwacollins@gmail.com>
2019-10-18 16:27:51 +0300

    Add login function plus auth/login.html template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms import (StringField, PasswordField, SubmitField, 
+                    ValidationError, BooleanField)
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 
@@ -20,3 +21,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError("That username is taken")
+
+class LoginForm(FlaskForm):
+    email = StringField("Your Email Address", validators=[Required(), Email()])
+    password = PasswordField("Password", validators=[Required()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,14 @@
-from flask import render_template, redirect, url_for
+from flask import (render_template, redirect, url_for,
+                  flash, request)
+from flask_login import login_user
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+# @auth.route('/login')
+# def login():
+#     return render_template('auth/login.html')
 
 @auth.route("/register", methods = ["GET", "POST"])
 def register():
@@ -20,4 +22,20 @@ def register():
     title = "New Account"
     return render_template("auth/register.html", 
                             registration_form = form,
+                            title = title)
+
+@auth.route("/login", methods = ["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get("next") or url_for(".index"))
+
+        flash("Invalid Username or Password")
+    
+    title = "Login to Watchlist"
+    return render_template("auth/login.html",
+                            login_form = login_form,
                             title = title)
