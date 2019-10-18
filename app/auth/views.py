7dc22a328748c772d2d2995716cd7460d2f3231c
Collins Muriuki <murerwacollins@gmail.com>
2019-10-18 16:57:59 +0300

    Add url_for('auth.logout')

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,7 +31,7 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
-            return redirect(request.args.get("next") or url_for(".index"))
+            return redirect(request.args.get("next") or url_for("main.index"))
 
         flash("Invalid Username or Password")
     
