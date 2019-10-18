521e085643e7372ed0584406836c2930c8c8d59d
Collins Muriuki <murerwacollins@gmail.com>
2019-10-18 11:44:26 +0300

    Db migration w/ UserMixin integration

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-login_manager = LoginManager()
+login_manager = LoginManager(
 login_manager.session_protection = "strong" #security level, monitor the changes in a user's request header and log the user out
 login_manager.login_view = "auth.login" #We prefix the login endpoint with the blueprint name because it is located inside a blueprint.
 
