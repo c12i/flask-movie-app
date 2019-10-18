eea34018dca3b287b9940de296f0e3439c7d3e8f
Collins Muriuki <murerwacollins@gmail.com>
2019-10-18 15:29:02 +0300

    Error of omission fix, complete registration route; still missing templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,7 +12,8 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data)
+        user = User(email = form.email.data, username = form.username.data,
+                    password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for(".login"))
