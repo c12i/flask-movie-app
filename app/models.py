86c1b4844b16f25b7ac6a0d8ce2e6ca2461bf67f
Collins Muriuki <murerwacollins@gmail.com>
2019-10-19 17:22:25 +0300

    Add bio and profile_pic_path columns to User model; db migration done

--- app/models.py
+++ app/models.py
@@ -53,6 +53,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique = True, index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
 
     @property
