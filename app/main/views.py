2368d3cbcc71d1ab1cac800c49ceea1456abb24e
Collins Muriuki <murerwacollins@gmail.com>
2019-10-19 18:50:23 +0300

    Create update_profile view function, update profile/profile html template and minor views change

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,10 @@ from flask import (render_template, request, redirect,
                    url_for, abort)
 from . import main
 from ..request import get_movies, get_movie, search_movie
-from .forms import ReviewForm
+from .forms import ReviewForm, UpdateProfile
 from ..models import Review, User
 from flask_login import login_required
+from .. import db
 
 # Views
 @main.route('/')
@@ -82,5 +83,27 @@ def profile(uname):
     if user is None:
         abort(404)
 
+    title = user.username
+
     return render_template("profile/profile.html",
-                            user = user)
+                            user = user,
+                            title = title)
+
+@main.route("/user/<uname>/update", methods = ["GET", "POST"])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("main.profile", 
+                                uname = user.username))
+
+    return render_template("profile/update.html",
+                            form = form)
