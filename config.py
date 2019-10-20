51eede90a43e263a7fd5623bd06abc7d9e020c57
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 22:36:47 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_PINK_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     """
