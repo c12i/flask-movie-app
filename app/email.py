fc9a113ee2048301b1a168bd53f6a26414e77fcd
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 21:55:34 +0300

    Solve smtp email blocker; remains disabled due to google security

--- app/email.py
+++ app/email.py
@@ -6,6 +6,6 @@ def mail_message(subject,template,to,**kwargs):
     sender_email = "murerwacollins@gmail.com"
 
     email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
+    email.body = render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
