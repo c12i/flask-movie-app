5800f7b194c50d7048604cc6dd8a25469aaff26e
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 07:39:59 +0300

    Create update_pic view function

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from ..request import get_movies, get_movie, search_movie
 from .forms import ReviewForm, UpdateProfile
 from ..models import Review, User
 from flask_login import login_required
-from .. import db
+from .. import db, photos
 
 # Views
 @main.route('/')
@@ -107,3 +107,15 @@ def update_profile(uname):
 
     return render_template("profile/update.html",
                             form = form)
+
+@main.route("/user/<uname>/update/pic", methods = ["POST"])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if "photo" in request.files:
+        filename = photos.save(request.files["photo"])
+        path = f"photos/{filename}"
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for("main.profile",
+                             uname=uname))
