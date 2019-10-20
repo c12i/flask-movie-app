fa393b43c69180c07c3d1669bd1b1c6e6911238e
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 20:04:48 +0300

    Implement simpleMDE form; currently saves review as markdown

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ login_manager.session_protection = "strong" #security level, monitor the changes
 login_manager.login_view = "auth.login" #We prefix the login endpoint with the blueprint name because it is located inside a blueprint.
 photos = UploadSet("photos", IMAGES)
 mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
     
@@ -26,6 +28,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    simple.init_app(app)
 
     # Registering the main app Blueprint
     from .main import main as main_blueprint
--- config.py
+++ config.py
@@ -17,6 +17,10 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+    # simplemde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 class ProdConfig(Config):
     '''
     Production  configuration child class
