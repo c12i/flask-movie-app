e3ae18dcf0cdff4f29c32c1ee9cd228a4923d809
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 13:59:35 +0300

    Update manager.shell context function

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User, Role
+from app.models import User, Role, Review
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app("development")
@@ -21,7 +21,11 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Role=Role)
+    return dict(app = app,
+                db = db,
+                User = User, 
+                Role = Role,
+                Review = Review)
 
 if __name__ == '__main__':
     manager.run()
