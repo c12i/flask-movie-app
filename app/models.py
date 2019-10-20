43064f56bccea889ac1882d41300c0a98b119ea8
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 13:50:18 +0300

    Add Review model; migrate database

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import (generate_password_hash,
                                check_password_hash)
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -21,31 +22,6 @@ class Movie:
         self.vote_average = vote_average
         self.vote_count = vote_count
 
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-    
-    @classmethod
-    def get_reviews(cls,id):
-        response = []
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-        return response
-
 class User(UserMixin, db.Model):
     # Ovewriting a the table name geneerated by SQLAlchemy
     __tablename__ = 'users'
@@ -56,6 +32,7 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
@@ -71,6 +48,27 @@ class User(UserMixin, db.Model):
     # string representaion to print out a row of a column, important in debugging
     def __repr__(self):
         return f'User {self.username}'
+
+class Review(db.Model):
+
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls,id):
+        reviews = Review.query.filter_by(movie_id=id).all()
+        return reviews
 
 class Role(db.Model):
     __tablename__ = 'roles'
