3059674a3cd6826409804db104f874cde676728c
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 21:31:18 +0300

    SMTP email config edit

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     UPLOADED_PHOTOS_DEST = "app/static/photos"
 
     # email configurations
-    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
