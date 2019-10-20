ea49c24ee537185008d7427f7a9c95c2979bbd52
Collins Muriuki <murerwacollins@gmail.com>
2019-10-20 11:47:21 +0300

    Create email.py module to handle email logic

---
+++ app/email.py
@@ -0,0 +1,11 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+def mail_message(subject,template,to,**kwargs):
+    sender_email = "murerwacollins@gmail.com"
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
+    mail.send(email)
