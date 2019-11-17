a2b570cae0164347429d7c008bb3cab843bfc8c7
Collins Muriuki <murerwacollins@gmail.com>
2019-11-17 09:35:26 +0300

    Set config options to development

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Server
 from app.models import User, Role, Review
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app("production")
+app = create_app("development")
 manager = Manager(app)
 manager.add_command("server", Server)
 
